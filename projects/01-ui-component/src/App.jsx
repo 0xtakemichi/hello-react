26a7f121c4eda1150fad3ad115d8f4b58a6448d3
0xtakemichi <fjerez034@gmail.com>
2025-06-10 23:53:33 -0400

    01: Renders dynamic Twitter follow cards

--- projects/01-ui-component/src/App.jsx
+++ projects/01-ui-component/src/App.jsx
@@ -1,16 +1,35 @@
 import './App.css'
 import TwitterFollowCard from './TwitterFollowCard'
 
+const users = [
+  {
+    userName: '37t',
+    name: 'Jorge',
+    isFollowing: true
+  },
+  {
+    userName: 'midudev',
+    name: 'Miguel',
+    isFollowing: false
+  }
+]
+
 function App() {
   return (
     <section className='App'>
-      <TwitterFollowCard userName="37t">
-        37t
-      </TwitterFollowCard>
-
-      <TwitterFollowCard userName="midudev">
-        midudev
-      </TwitterFollowCard>
+      {
+        users.map(({ userName, name, isFollowing }) => {
+          return (
+            <TwitterFollowCard 
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+              >
+                {name}
+              </TwitterFollowCard>
+          )
+        })
+      }
     </section>
   )
 }
