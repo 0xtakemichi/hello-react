2b4d2fa934a79e0286ac519412090c0050894381
0xtakemichi <fjerez034@gmail.com>
2025-06-17 22:55:59 -0400

    02: Implement winner detection

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -6,6 +6,17 @@ const TURNS = {
     O: '⭕'
 }
 
+const WINNERS_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
 const Square = ({ children, isSelected, updateBoard, index }) => {
 
   const className = `square ${isSelected ? 'is-selected' : ''}`
@@ -24,16 +35,35 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNERS_COMBINATIONS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
 
   const updateBoard = (index) => {
+    if (board[index] || winner) return
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-    console.log(board)
-    console.log(newBoard)
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   return (
@@ -62,6 +92,18 @@ function App() {
           {TURNS.O}
         </Square> 
       </section>
+      {
+        winner && (
+          <section className='winner'>
+            <div className='text'>
+              <div className='win'>
+                {winner} wins!
+              </div>
+              <button onClick={() => setBoard(Array(9).fill(null))}>Play again</button>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
