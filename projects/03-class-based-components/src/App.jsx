2cdc754f3ef2f1890197e35ca778abccd63b3535
0xtakemichi <fjerez034@gmail.com>
2025-06-29 18:24:16 -0400

    03: add lifecycle methods to Button and update App state logic

--- projects/03-class-based-components/src/App.jsx
+++ projects/03-class-based-components/src/App.jsx
@@ -2,28 +2,50 @@ import "./App.css";
 import { Component } from "react";
 
 class Button extends Component {
+  state = {}
+  constructor(props) {
+    super(props)
+    console.log('constructor', props)
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate', prevProps, prevState)
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount', this.props, this.state)
+  }
+
   render() {
     console.log('Ejecutando metodo render de button')
     return (
-      <button>
+      <button onClick={() => this.setState({ prop: 1})}>
         Enviar
       </button>
     )
   }
 }
+
 class App extends Component {
   state = {
-    valor: 0
+    valor: 3
   }
   render() {
     console.log(this.state);
     return (
       <div>
         <p>Hello World</p>
-        <Button />
+        {this.state.valor === 3
+        ? <Button plague="Hellow world"/>
+        : null}
+
         <button 
           className={`${this.state.valor}`} 
-          onClick={() => this.setState({ valor: 24 })}
+          onClick={() => this.setState({ valor: 5 })}
         >
           Enviar en App
         </button>
