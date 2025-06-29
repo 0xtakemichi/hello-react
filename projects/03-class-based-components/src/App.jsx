755d04499df9b17468006704cb0eba76bab8375f
0xtakemichi <fjerez034@gmail.com>
2025-06-29 17:55:39 -0400

    03: add Button component and button with state update to App component

--- projects/03-class-based-components/src/App.jsx
+++ projects/03-class-based-components/src/App.jsx
@@ -1,14 +1,35 @@
-import './App.css'
-import {Component} from 'react'
+import "./App.css";
+import { Component } from "react";
 
-class App extends Component {
+class Button extends Component {
   render() {
+    console.log('Ejecutando metodo render de button')
     return (
-      <h1>
-        Hello World
-      </h1>
+      <button>
+        Enviar
+      </button>
     )
   }
 }
+class App extends Component {
+  state = {
+    valor: 0
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <p>Hello World</p>
+        <Button />
+        <button 
+          className={`${this.state.valor}`} 
+          onClick={() => this.setState({ valor: 24 })}
+        >
+          Enviar en App
+        </button>
+      </div>
+    );
+  }
+}
 
-export default App
+export default App;
