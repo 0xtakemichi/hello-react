4ab0a2cc4d17f4554323cc48b176e4f14e80dd87
0xtakemichi <fjerez034@gmail.com>
2025-06-29 17:18:52 -0400

    00: add conditional rendering and array mapping to main.tsx

--- projects/00-hola-mundo/src/main.tsx
+++ projects/00-hola-mundo/src/main.tsx
@@ -1,28 +1,30 @@
 // main.tsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Button from './components/Button'
-import './main.css'
-import appleIcon from './assets/apple.svg'
-import googleIcon from './assets/google.svg'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Button from "./components/Button";
+import "./main.css";
+import appleIcon from "./assets/apple.svg";
+import googleIcon from "./assets/google.svg";
+
+const arr: string[] = ["felipe", "takemi", "plague"];
+const myVariable: boolean = false;
 
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <h1>Hello World</h1>
-    <Button 
-    onClick={() => console.log('Apple')}
-    icon={appleIcon}
-    className='m-2'
-    > 
+
+    <Button onClick={() => console.log("Apple")} icon={appleIcon}>
       Apple
     </Button>
-    <Button 
-    onClick={() => console.log('Google')}
-    icon={googleIcon}
-    className='m-2'
-    > 
+
+    <Button onClick={() => console.log("Google")} icon={googleIcon}>
       Google
     </Button>
-  </StrictMode>,
-)
+
+    {myVariable && <p className="text-red-500">myVariable is true</p>}
+    {!myVariable && <p className="text-green-500">myVariable is false</p>}
+
+    {arr.map(item => <p key={item}>{item}</p>)}
+  </StrictMode>
+);
