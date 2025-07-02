383fdcb616ff353e308389a4e8feb1d8e4146262
0xtakemichi <fjerez034@gmail.com>
2025-07-01 21:12:30 -0400

    04: create shopping-cart

---
+++ projects/04-shopping-cart/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Tienda de productos para mascotas</h1>
+        <p>Bienvenido a nuestra tienda de productos para mascotas</p>
+      </div>
+      <div>
+        <h2>Productos</h2>
+        <p>Aquí puedes encontrar nuestros productos</p>
+      </div>
+    </>
+  )
+}
+
+export default App
