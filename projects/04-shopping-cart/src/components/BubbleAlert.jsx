5f79b807722f9d3899ee799f47b3674bc8dfbe79
0xtakemichi <fjerez034@gmail.com>
2025-07-03 18:35:21 -0400

    04: add dynamic quantity to BubbleAlert component

--- projects/04-shopping-cart/src/components/BubbleAlert.jsx
+++ projects/04-shopping-cart/src/components/BubbleAlert.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function BubbleAlert() {
+function BubbleAlert({n}) {
+  const getQuantity = (n) => {
+    return n > 9 ? '9+' : n;
+  }
   return (
-    <span className="relative">
+    <span className="relative left-2 top-2">
         <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1.5 text-xs font-medium leading-3 text-white bg-blue-600 rounded-full ring-1 ring-blue-600/20">
-            1
+            {getQuantity(n)}
         </span>
     </span>
   )
--- projects/04-shopping-cart/src/components/Carro.jsx
+++ projects/04-shopping-cart/src/components/Carro.jsx
@@ -4,7 +4,7 @@ function Carro() {
   return (
     <div>
         <span className="relative">
-            <BubbleAlert />
+            <BubbleAlert n={10}/>
         </span>
         <button 
             className="bg-blue-500 text-white p-2 mt-2 rounded-lg cursor-pointer hover:bg-blue-600"
