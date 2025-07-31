ae4a51c2720977ddd2ca95194d99f107d36fdde2
0xtakemichi <fjerez034@gmail.com>
2025-07-30 21:17:24 -0400

    05: refactor mouse follower into separate component

--- projects/05-mouse-follower/src/App.jsx
+++ projects/05-mouse-follower/src/App.jsx
@@ -1,46 +1,10 @@
-import { useEffect, useState } from 'react'
+import FollowMouse from './components/follow-mouse'
 
 function App() {
-  const [enabled, setEnabled] = useState(false)
-  const [position, setPosition] = useState({ x: 0, y: 0 })
-
-  useEffect(() => {
-    console.log({ enabled })
-    const handleMouseMove = (event) => {
-      const { clientX, clientY } = event
-      console.log(`X=${clientX}, Y=${clientY}`)
-      setPosition({ x: clientX, y: clientY })
-    }
-    if (enabled) {
-      window.addEventListener('pointermove', handleMouseMove)
-    }
-    return () => {
-      window.removeEventListener('pointermove', handleMouseMove)
-      setPosition({ x: 0, y: 0 })
-    }
-  }, [enabled])
-
   return (
-    <>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}
-      />
-      <h1>Mouse Follower</h1>
-      <button onClick={() => setEnabled(!enabled)}>
-        {enabled ? 'Disable' : 'Enable'} Mouse Follower
-      </button>
-    </>
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
