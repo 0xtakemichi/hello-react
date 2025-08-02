3dffa6aae0d91a3af3f42c5012b5a71b773d712a
0xtakemichi <fjerez034@gmail.com>
2025-08-01 22:49:07 -0400

    06: show cat image based on first three words of fact

--- projects/06-prueba-tecnica/src/App.jsx
+++ projects/06-prueba-tecnica/src/App.jsx
@@ -5,17 +5,31 @@ const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
 function App() {
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact, data.length))
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const threeFirstWords = fact.split(' ', 3).join(' ')
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImageUrl(url)
+          })
+      })
   }, [])
 
   return (
     <main>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt="Image extracted using the first three words of the fact" />}
     </main>
   )
 }
