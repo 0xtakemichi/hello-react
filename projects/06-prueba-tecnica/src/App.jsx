ce333f19360acaace02175fed12ac8992cda7df2
0xtakemichi <fjerez034@gmail.com>
2025-08-02 21:27:27 -0400

    06: refactor image fetch logic to use separate effect

--- projects/06-prueba-tecnica/src/App.jsx
+++ projects/06-prueba-tecnica/src/App.jsx
@@ -8,23 +8,27 @@ function App() {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
 
+  // para recuperar la cita al cargar la aplicación
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
       .then(data => {
         const { fact } = data
         setFact(fact)
-
-        const threeFirstWords = fact.split(' ', 3).join(' ')
-
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response
-            setImageUrl(url)
-          })
       })
   }, [])
+
+  // para recuperar la imagen cada vez que tenemos una cita nueva
+  useEffect(() => {
+    if (!fact) return
+    const threeFirstWords = fact.split(' ', 3).join(' ')
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
 
   return (
     <main>
