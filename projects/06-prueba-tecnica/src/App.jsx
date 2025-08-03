d68e56ddaddf2e7276a1e2ef8515811cc1b95a51
0xtakemichi <fjerez034@gmail.com>
2025-08-02 20:41:35 -0400

    06: add CSS styles and update layout structure

--- projects/06-prueba-tecnica/src/App.jsx
+++ projects/06-prueba-tecnica/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
@@ -28,8 +29,10 @@ function App() {
   return (
     <main>
       <h1>App de gatitos</h1>
-      {fact && <p>{fact}</p>}
-      {imageUrl && <img src={imageUrl} alt="Image extracted using the first three words of the fact" />}
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={imageUrl} alt="Image extracted using the first three words of the fact" />}
+      </section>
     </main>
   )
 }
