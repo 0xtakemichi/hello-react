01e072677ef9f09f02b2fa8e2e3f59e42243223f
0xtakemichi <fjerez034@gmail.com>
2025-08-04 22:09:59 -0400

    06: refactor cat image logic into custom hook

--- projects/06-prueba-tecnica/src/App.jsx
+++ projects/06-prueba-tecnica/src/App.jsx
@@ -1,29 +1,16 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { getRandomFact } from './services/facts'
-
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+import { useCatImage } from './hooks/useCatImage'
 
 function App() {
   const [fact, setFact] = useState()
-  const [imageUrl, setImageUrl] = useState()
+  const { imageUrl } = useCatImage({ fact })
 
   // para recuperar la cita al cargar la aplicación
   useEffect(() => {
     getRandomFact().then(newFact => setFact(newFact))
   }, [])
-
-  // para recuperar la imagen cada vez que tenemos una cita nueva
-  useEffect(() => {
-    if (!fact) return
-    const threeFirstWords = fact.split(' ', 3).join(' ')
-    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-      .then(res => res.json())
-      .then(response => {
-        const { url } = response
-        setImageUrl(url)
-      })
-  }, [fact])
 
   const handleClick = async () => {
     const newFact = await getRandomFact()
