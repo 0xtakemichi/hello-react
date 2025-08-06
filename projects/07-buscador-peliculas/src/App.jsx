c06601fd8561204a2b1ef104c708d085db34ec64
0xtakemichi <fjerez034@gmail.com>
2025-08-05 21:36:04 -0400

    07: Create Movie Finder

---
+++ projects/07-buscador-peliculas/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='page'>
+        <header>
+          <h1>Movie Search</h1>
+          <form action="" className="form">
+            <input type="text" placeholder="Search for a movie..." />
+            <button type="submit">Search</button>
+          </form>
+        </header>
+
+        <main>
+          <h2>Search Results</h2>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
