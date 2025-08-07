1593493b6bd281c60cb31e51e909dc0c0d9c08c8
0xtakemichi <fjerez034@gmail.com>
2025-08-06 22:08:53 -0400

    07: extract movie list and empty state into reusable components

--- projects/07-buscador-peliculas/src/App.jsx
+++ projects/07-buscador-peliculas/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import { Movies } from './components/Movies'
 import resposeMovies from './mocks/with-results.json'
 import withoutResults from './mocks/without-results.json'
 
 function App() {
   const movies = resposeMovies.Search
-  const hasMovies = movies?.length > 0
 
   return (
     <>
@@ -19,21 +19,7 @@ function App() {
 
         <main>
           <h2>Search Results</h2>
-          {
-            hasMovies ? (
-              <ul>
-                {movies.map(movie => (
-                  <li key={movie.imdbID}>
-                    <h3>{movie.Title}</h3>
-                    <p>{movie.Year}</p>
-                    <img src={movie.Poster} alt={movie.Title} />
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>No results found.</p>
-            )
-          }
+          <Movies movies={movies} />
         </main>
       </div>
     </>
