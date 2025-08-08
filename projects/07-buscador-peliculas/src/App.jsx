87f95a9cb5f114bef831e3324b9c43db8a993598
0xtakemichi <fjerez034@gmail.com>
2025-08-07 22:15:13 -0400

    07: add search form with submit handler to movie app

--- projects/07-buscador-peliculas/src/App.jsx
+++ projects/07-buscador-peliculas/src/App.jsx
@@ -3,22 +3,32 @@ import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
 
 function App() {
-  const { movies: mappedMovies } = useMovies()
+  const { movies } = useMovies()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const { query } = Object.fromEntries(
+      new window.FormData(event.target)
+    )
+    if (!query) {
+      return
+    }
+  }
 
   return (
     <>
       <div className='page'>
         <header>
           <h1>Movie Search</h1>
-          <form action="" className="form">
-            <input type="text" placeholder="Search for a movie..." />
+          <form className="form" onSubmit={handleSubmit}>
+            <input name='query' placeholder="Search for a movie..." />
             <button type="submit">Search</button>
           </form>
         </header>
 
         <main>
           <h2>Search Results</h2>
-          <Movies movies={mappedMovies} />
+          <Movies movies={movies} />
         </main>
       </div>
     </>
