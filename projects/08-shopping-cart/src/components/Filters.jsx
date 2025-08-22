26b891a04513c6e84a3490b46d1533eeeab97ee7
0xtakemichi <fjerez034@gmail.com>
2025-08-21 23:32:13 -0400

    08: add unique IDs to filter inputs using useId

--- projects/08-shopping-cart/src/components/Filters.jsx
+++ projects/08-shopping-cart/src/components/Filters.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useId } from 'react'
 import './Filters.css'
 
 export default function Filters({ onChange }) {
     const [minPrice, setMinPrice] = useState(0)
+    const minPriceFilterId = useId()
+    const categoryFilterId = useId()
 
     const handleChangeMinPrice = (event) => {
         setMinPrice(event.target.value)
@@ -23,8 +25,8 @@ export default function Filters({ onChange }) {
         <section className="filters">
 
             <div>
-                <label htmlFor="category">Category</label>
-                <select id="category" onChange={handleChangeCategory}>
+                <label htmlFor={categoryFilterId}>Category</label>
+                <select id={categoryFilterId} onChange={handleChangeCategory}>
                     <option value="all">All</option>
                     <option value="laptops">Laptops</option>
                     <option value="smartphones">Smartphones</option>
@@ -32,8 +34,8 @@ export default function Filters({ onChange }) {
             </div>
 
             <div>
-                <label htmlFor="price">Min price</label>
-                <input type="range" id="price" min="0" max="2000" onChange={handleChangeMinPrice} value={minPrice} />
+                <label htmlFor={minPriceFilterId}>Min price</label>
+                <input type="range" id={minPriceFilterId} min="0" max="2000" onChange={handleChangeMinPrice} value={minPrice} />
                 <span>$ {minPrice}</span>
             </div>
 
