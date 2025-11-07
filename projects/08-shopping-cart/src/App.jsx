4df93d7904a00f4cf199a03c54481828166bd2cd
0xtakemichi <fjerez034@gmail.com>
2025-11-06 23:29:02 -0300

    refactor: useFilters hook into separate file
    Moved the useFilters hook from App.jsx to a new file at hooks/useFilters.js for better code organization and reusability. Updated imports in App.jsx to use the new hook location.

--- projects/08-shopping-cart/src/App.jsx
+++ projects/08-shopping-cart/src/App.jsx
@@ -4,25 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import { useState } from 'react'
 import { IS_DEVELOPMENT } from '../config'
-import { useContext } from 'react'
-import { FiltersContext } from './context/filters'
-
-function useFilters () {
-  const {filters, setFilters} = useContext(FiltersContext)
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice &&
-        (
-          filters.category === 'all' ||
-          product.category === filters.category
-        )
-      )
-    })
-  }
-  return {filters, filterProducts, setFilters}
-}
+import { useFilters } from './hooks/useFilters'
 
 function App() {
   const [products] = useState(initialProducts)
