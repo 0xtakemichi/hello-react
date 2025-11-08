fe7653426019293074b04fea905b3bb6086e632a
0xtakemichi <fjerez034@gmail.com>
2025-11-07 23:40:56 -0300

    08: refactor Footer to use useFilters hook directly

--- projects/08-shopping-cart/src/components/Footer.jsx
+++ projects/08-shopping-cart/src/components/Footer.jsx
@@ -1,6 +1,10 @@
+import { useFilters } from '../hooks/useFilters'
 import './Footer.css'
 
-function Footer ({ filters }) {
+function Footer () {
+
+    const { filters } = useFilters
+
     return (
         <footer className='footer'>
             {
