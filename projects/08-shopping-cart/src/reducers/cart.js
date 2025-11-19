a08f9581ad6da1c803e63d0590088039173de320
0xtakemichi <fjerez034@gmail.com>
2025-11-18 22:35:08 -0300

    08: refactor cartReducer to use action handler map

--- projects/08-shopping-cart/src/reducers/cart.js
+++ projects/08-shopping-cart/src/reducers/cart.js
@@ -12,37 +12,58 @@ export const updateLocalStorage = (state) => {
   window.localStorage.setItem("cart", JSON.stringify(state));
 };
 
+const UPDATE_STATE_BY_ACTION = {
+  [CART_ACTIONS_TYPES.ADD_TO_CART]: (state, action) => {
+    const { id } = action.payload;
+    const productInCartIndex = state.findIndex((item) => item.id === id);
+
+    if (productInCartIndex >= 0) {
+      // 📝 usando structuredClone
+      //   const newState = structuredClone(state);
+      //   newState[productInCartIndex].quantity += 1;
+
+      // 😌 usando map
+      // const newState = state.map((item) => {
+      //   if (item.id === id) {
+      //     return { ...item, quantity: item.quantity + 1 };
+      //   }
+      //   return item;
+      // });
+
+      // 🚀 usando spread operator y slice
+      const newState = [
+        ...state.slice(0, productInCartIndex),
+        {
+          ...state[productInCartIndex],
+          quantity: state[productInCartIndex].quantity + 1,
+        },
+        ...state.slice(productInCartIndex + 1),
+      ];
+
+      updateLocalStorage(newState);
+      return newState;
+    }
+
+    const newState = [...state, { ...action.payload, quantity: 1 }];
+    updateLocalStorage(newState);
+    return newState;
+  },
+
+  [CART_ACTIONS_TYPES.REMOVE_FROM_CART]: (state, action) => {
+    const { id } = action.payload;
+    const newState = state.filter((item) => item.id !== id);
+    updateLocalStorage(newState);
+    return newState;
+  },
+
+  [CART_ACTIONS_TYPES.CLEAR_CART]: () => {
+    updateLocalStorage([]);
+    return [];
+  },
+};
+
 export const cartReducer = (state, action) => {
-  const { type: actionType, payload: actionPayload } = action;
-
-  switch (actionType) {
-    case CART_ACTIONS_TYPES.ADD_TO_CART: {
-      const { id } = actionPayload;
-      const productInCartIndex = state.findIndex((item) => item.id === id);
-
-      if (productInCartIndex >= 0) {
-        const newState = structuredClone(state);
-        newState[productInCartIndex].quantity += 1;
-        updateLocalStorage(newState);
-        return newState;
-      }
-
-      const newState = [...state, { ...actionPayload, quantity: 1 }];
-      updateLocalStorage(newState);
-      return newState;
-    }
-
-    case CART_ACTIONS_TYPES.REMOVE_FROM_CART: {
-      const { id } = actionPayload;
-      const newState = state.filter((item) => item.id !== id);
-      updateLocalStorage(newState);
-      return newState;
-    }
-
-    case CART_ACTIONS_TYPES.CLEAR_CART: {
-      updateLocalStorage(cartInitialState);
-      return cartInitialState;
-    }
-  }
-  return state;
+  const { type: actionType } = action;
+  const updateState = UPDATE_STATE_BY_ACTION[actionType];
+  return updateState ? updateState(state, action) : state;
 };
