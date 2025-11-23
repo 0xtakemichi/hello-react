6bc576cd02087c77e277bc38b2989b57ea4835c0
0xtakemichi <fjerez034@gmail.com>
2025-11-22 23:57:50 -0300

    09: add declarative Router and custom 404 page

--- projects/09-yoru-router/src/App.jsx
+++ projects/09-yoru-router/src/App.jsx
@@ -1,39 +1,18 @@
-import { useEffect, useState } from "react";
-import { EVENTS } from "./const";
 import HomePage from "./pages/Home";
 import AboutPage from "./pages/About";
+import { Router } from "./Router";
+import Page404 from "./pages/404";
 
-const routes = [
+const appRoutes = [
   { path: "/", Component: HomePage },
   { path: "/about", Component: AboutPage },
+  { path: "/contact", Component: () => <h1>Contact</h1> },
 ];
-
-function Router({
-  routes = [],
-  defaultComponent: DefaultComponent = () => <h1>404</h1>,
-}) {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
-
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname);
-    };
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange);
-    // Para volver atras con navegacion del navegador
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange);
-    return () => {
-      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange);
-      window.removeEventListener(EVENTS.POPSTATE, onLocationChange);
-    };
-  }, []);
-  const Page = routes.find(({ path }) => path === currentPath)?.Component;
-  return Page ? <Page /> : <DefaultComponent />;
-}
 
 function App() {
   return (
     <main>
-      <Router routes={routes} />
+      <Router routes={appRoutes} defaultComponent={Page404} />
     </main>
   );
 }
