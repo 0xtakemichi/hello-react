09428d5a0320717a8fdc8a5efdd84e5f31c3016d
0xtakemichi <fjerez034@gmail.com>
2025-11-23 23:50:48 -0300

    09: add dynamic search route with path-to-regexp

--- projects/09-yoru-router/src/App.jsx
+++ projects/09-yoru-router/src/App.jsx
@@ -1,5 +1,6 @@
 import HomePage from "./pages/Home";
 import AboutPage from "./pages/About";
+import SearchPage from "./pages/Search";
 import { Router } from "./Router";
 import Page404 from "./pages/404";
 
@@ -7,6 +8,10 @@ const appRoutes = [
   { path: "/", Component: HomePage },
   { path: "/about", Component: AboutPage },
   { path: "/contact", Component: () => <h1>Contact</h1> },
+  {
+    path: "/search/:query",
+    Component: SearchPage,
+  },
 ];
 
 function App() {
--- projects/09-yoru-router/src/Router.jsx
+++ projects/09-yoru-router/src/Router.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { EVENTS } from "./const";
+import { match } from "path-to-regexp";
 
 export function Router({
   routes = [],
@@ -19,6 +20,28 @@ export function Router({
       window.removeEventListener(EVENTS.POPSTATE, onLocationChange);
     };
   }, []);
-  const Page = routes.find(({ path }) => path === currentPath)?.Component;
-  return Page ? <Page /> : <DefaultComponent />;
+
+  let routeParams = {};
+
+  const Page = routes.find(({ path }) => {
+    if (path === currentPath) return true;
+
+    // usando path-to-regexp
+    // para poder detectar rutas con parametros (rutas dinamicas)
+    // /search/:query
+    const matcherUrl = match(path, { decode: decodeURIComponent });
+    const matched = matcherUrl(currentPath);
+    if (!matched) return false;
+
+    // guardar los parametros de la la url que eran dinamicos
+    // y que hemos extraido con path-to-regexp
+    routeParams = matched.params;
+    return true;
+  })?.Component;
+
+  return Page ? (
+    <Page routeParams={routeParams} />
+  ) : (
+    <DefaultComponent routeParams={routeParams} />
+  );
 }
