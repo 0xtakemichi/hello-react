128a527f45e38ae21805d9e64a571591014e96cd
0xtakemichi <fjerez034@gmail.com>
2025-11-30 23:18:47 -0300

    10: add Todos component and integrate with App

--- projects/10-todo-app-ts/src/App.tsx
+++ projects/10-todo-app-ts/src/App.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Todos } from "./components/Todos";
+
+const mockTodos = [
+  {
+    id: "1",
+    title: "Todo 1",
+    completed: false,
+  },
+  {
+    id: "2",
+    title: "Todo 2",
+    completed: false,
+  },
+  {
+    id: "3",
+    title: "Todo 3",
+    completed: true,
+  },
+];
 
 const App: React.FC = () => {
-  return (
-    <>
-      <h1>Todo App with TypeScript</h1>
-    </>
-  );
+  const [todos] = useState(mockTodos);
+  return <Todos todos={todos} />;
 };
 
 export default App;
