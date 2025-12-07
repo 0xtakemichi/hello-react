984f21201fb34a05a2b3c7b5ab6d690a5fdca35c
0xtakemichi <fjerez034@gmail.com>
2025-12-06 22:37:41 -0300

    10: add toggle complete functionality to todos

--- projects/10-todo-app-ts/src/components/Todos.tsx
+++ projects/10-todo-app-ts/src/components/Todos.tsx
@@ -1,12 +1,20 @@
-import { type ListOfTodos, type TodoId } from "../types";
+import { type ListOfTodos, type TodoId, type Todo as TodoType } from "../types";
 import { Todo } from "./Todo";
 
 interface Props {
   todos: ListOfTodos;
   onRemoveTodo: ({ id }: TodoId) => void;
+  onToggleCompleteTodo: ({
+    id,
+    completed,
+  }: Pick<TodoType, "id" | "completed">) => void;
 }
 
-export const Todos: React.FC<Props> = ({ todos, onRemoveTodo }) => {
+export const Todos: React.FC<Props> = ({
+  todos,
+  onRemoveTodo,
+  onToggleCompleteTodo,
+}) => {
   return (
     <ul className="todo-list">
       {todos.map((todo) => (
@@ -16,6 +24,7 @@ export const Todos: React.FC<Props> = ({ todos, onRemoveTodo }) => {
             id={todo.id}
             title={todo.title}
             completed={todo.completed}
+            onToggleCompleteTodo={onToggleCompleteTodo}
             onRemoveTodo={onRemoveTodo}
           />
         </li>
