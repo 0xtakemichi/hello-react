19125490347657781e81767a451c0e363f78c58a
0xtakemichi <fjerez034@gmail.com>
2025-12-28 00:02:15 -0300

    11: add reducer and state types for translation app

--- projects/11-google-translate-clone/src/App.tsx
+++ projects/11-google-translate-clone/src/App.tsx
@@ -1,6 +1,59 @@
+import { useReducer } from "react";
 import "./App.css";
+import type { State } from "./types";
+
+// 1. Create a initialState
+const initialState: State = {
+  fromLanguage: "auto",
+  toLanguage: "en",
+  fromText: "",
+  result: "",
+  loading: false,
+};
+
+// 2. Create a reducer
+function reducer(state: State, action) {
+  const { type, payload } = action;
+  if (type === "INTERCHANGE_LANGUAGES") {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage,
+    };
+  }
+  if (type === "SET_FROM_LANGUAGE") {
+    return {
+      ...state,
+      fromLanguage: payload,
+    };
+  }
+  if (type === "SET_TO_LANGUAGE") {
+    return {
+      ...state,
+      toLanguage: payload,
+    };
+  }
+  if (type === "SET_FROM_TEXT") {
+    return {
+      ...state,
+      loading: true,
+      fromText: payload,
+      result: "",
+    };
+  }
+  if (type === "SET_RESULT") {
+    return {
+      ...state,
+      loading: false,
+      result: payload,
+    };
+  }
+  return state;
+}
 
 function App() {
+  // 3. Use useReducer hook to manage state
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
       <div className="App">
